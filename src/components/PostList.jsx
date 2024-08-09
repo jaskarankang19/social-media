2a756fdcb9a5e1179d2d52f0c9a5ec608d63d49e
jaskarankang19 <73731645+jaskarankang19@users.noreply.github.com>
2024-08-08 21:29:30 -0400

    add spinner and clean up effect

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,24 +1,33 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostListContext } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListContext);
+  const [fetching, setFetching] = useState(false);
 
   useEffect(() => {
-    fetch("https://dummyjson.com/posts")
+    setFetching(true);
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch("https://dummyjson.com/posts", { signal })
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
+        setFetching(false);
       });
+    return () => {
+      controller.abort();
+    };
   }, []);
 
   return (
     <>
-      {postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {fetching && <LoadingSpinner></LoadingSpinner>}
+      {!fetching && postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
